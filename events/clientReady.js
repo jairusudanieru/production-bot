4d1838b8a1008182ddabd320ad59e2bae91a01f8
Jairusu <jairusudanieru@gmail.com>
2026-04-02 22:21:17 +0800

    added auto delete for old projects data

--- events/clientReady.js
+++ events/clientReady.js
@@ -1,19 +1,38 @@
 const { ActivityType, Events } = require("discord.js");
 
+const DatabaseManager = require("../managers/databaseManager.js");
+
+async function setPresence(client) {
+    await client.user.setPresence({
+        activities: [{
+            type: ActivityType.Custom,
+            name: 'custom',
+            state: ' ',
+        }],
+        status: 'online'
+    });
+}
+
+function startCleanupTask() {
+    const runCleanup = () => {
+        const deleted = DatabaseManager.cleanup(30);
+        console.log(`\nCleaned up ${deleted} old projects.`);
+    };
+
+    runCleanup();
+
+    setInterval(runCleanup, 1000 * 60 * 60 * 24);
+}
+
 module.exports = {
     name: Events.ClientReady,
     once: true,
 
     async execute(client) {
-        await client.user.setPresence({
-            activities: [{
-                type: ActivityType.Custom,
-                name: 'custom',
-                state: ' ',
-            }],
-            status: 'online'
-        });
+        await setPresence(client);
+        
+        console.log(`Ready! logged in as ${client.user.tag}`);
 
-        console.log(`Ready! logged in as ${client.user.tag}`)
+        startCleanupTask();
     },
 };
