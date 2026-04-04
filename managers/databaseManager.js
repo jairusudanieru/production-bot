7fe800bc27bc23243bd133e9ec1e35c9f210d270
Jairusu <jairusudanieru@gmail.com>
2026-04-04 23:16:17 +0800

    added function to prevent issues when no db found

--- managers/databaseManager.js
+++ managers/databaseManager.js
@@ -1,7 +1,9 @@
-const path = require('path');
-const Database = require('better-sqlite3');
+const fs = require("fs");
+const path = require("path");
+const Database = require("better-sqlite3");
 
-const dbPath = path.join(__dirname, '../database/projects.db');
+const dbPath = path.join(__dirname, "../database/projects.db");
+fs.mkdirSync(path.dirname(dbPath), { recursive: true });
 const db = new Database(dbPath);
 
 db.pragma('journal_mode = WAL');
